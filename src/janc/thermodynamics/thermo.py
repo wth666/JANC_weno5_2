79fa3b6382d379bab08c3f97ab0b7c99aa83c11e
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-23 13:31:38 +0800

    Update thermo.py

--- src/janc/thermodynamics/thermo.py
+++ src/janc/thermodynamics/thermo.py
@@ -148,7 +148,8 @@ def get_T_nasa7(e,Y,initial_T):
 
     def body_fun(args):
         res, de_dT, d2e_dT2, T, gamma, i = args
-        delta_T = -2*res*de_dT/(2*jnp.power(de_dT,2)-res*d2e_dT2)
+        #delta_T = -2*res*de_dT/(2*jnp.power(de_dT,2)-res*d2e_dT2)
+        delta_T = -res/de_dT
         T_new = T + delta_T
         # 限制温度范围，比如 T_min=200, T_max=5000 (根据你的物理范围调整)
         T_min = 0
