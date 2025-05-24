425b7f465d913673d4cf2214e5425dcd8f59f711
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-24 12:43:51 +0800

    Update thermo.py

--- src/janc/thermodynamics/thermo.py
+++ src/janc/thermodynamics/thermo.py
@@ -224,20 +224,21 @@ def get_T_nasa7(e,Y,initial_T):
     initial_res, initial_de_dT, initial_d2e_dT2, initial_gamma = e_eqn(initial_T,e,Y)
 
     def cond_fun(args):
-        res, de_dT, d2e_dT2, T, gamma, i = args
-        return (jnp.max(jnp.abs(res)) > tol) & (i < max_iter)
+        res, de_dT, d2e_dT2, T, gamma, dT, i = args
+        return (jnp.max(jnp.abs(dT)) > tol) & (i < max_iter)
 
     def body_fun(args):
-        res, de_dT, d2e_dT2, T, gamma, i = args
+        res, de_dT, d2e_dT2, T, gamma, dT, i = args
         delta_T = -2*res*de_dT/(2*jnp.power(de_dT,2)-res*d2e_dT2)
         T_new = T + delta_T
+        dT  = deta_T/T
         res_new, de_dT_new, d2e_dT2_new, gamma_new = e_eqn(T_new,e,Y)
-        return res_new, de_dT_new, d2e_dT2_new, T_new, gamma_new, i + 1
+        return res_new, de_dT_new, d2e_dT2_new, T_new, gamma_new, dT, i + 1
 
-    initial_state = (initial_res, initial_de_dT, initial_d2e_dT2, initial_T, initial_gamma, 0)
-    _, _, _, T_final, gamma_final, it = lax.while_loop(cond_fun, body_fun, initial_state)
+    initial_state = (initial_res, initial_de_dT, initial_d2e_dT2, initial_T, initial_gamma, 0.0, 0)
+    _, _, _, T_final, gamma_final, _, it = lax.while_loop(cond_fun, body_fun, initial_state)
     # 限制最小温度
-    T_final = jnp.clip(T_final, a_min=0.2)
+    #T_final = jnp.clip(T_final, a_min=0.2)
     #def print_warning(_):
         #debug.print("get_T_nasa7: 超过最大迭代步数")
         #return 0  # 返回一个占位值
