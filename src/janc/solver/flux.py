1f218406b5d6f851cd8855aeefe180482ee4c290
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-26 09:32:31 +0800

    Update flux.py

--- src/janc/solver/flux.py
+++ src/janc/solver/flux.py
@@ -358,3 +358,257 @@ def weno5_amr(field,dx,dy):
     netflux = dF/dx + dG/dy
 
     return -netflux
+
+
+
+
+
+
+
+
+###################HLLC#######################
+@jit
+def WENO_L_x(f):
+    fj = f[:,2:-3,3:-3]
+    fjp1 = f[:,3:-2,3:-3]
+    fjp2 = f[:,4:-1,3:-3]
+    fjm1 = f[:,1:-4,3:-3]
+    fjm2 = f[:,0:-5,3:-3]
+
+    IS1 = 1 / 4 * jnp.power((fjm2 - 4 * fjm1 + 3 * fj), 2) + 13 / 12 * jnp.power((fjm2 - 2 * fjm1 + fj), 2)
+    IS2 = 1 / 4 * jnp.power((fjm1 - fjp1), 2) + 13 / 12 * jnp.power((fjm1 - 2 * fj + fjp1), 2)
+    IS3 = 1 / 4 * jnp.power((3 * fj - 4 * fjp1 + fjp2), 2) + 13 / 12 * jnp.power((fj - 2 * fjp1 + fjp2), 2)
+
+    alpha1 = C1 / jnp.power((eps + IS1), p)
+    alpha2 = C2 / jnp.power((eps + IS2), p)
+    alpha3 = C3 / jnp.power((eps + IS3), p)
+
+
+    w1 = alpha1 / (alpha1 + alpha2 + alpha3)
+    w2 = alpha2 / (alpha1 + alpha2 + alpha3)
+    w3 = alpha3 / (alpha1 + alpha2 + alpha3)
+
+    fj_halfp1 = 1 / 3 * fjm2 - 7 / 6 * fjm1 + 11 / 6 * fj
+    fj_halfp2 = -1 / 6 * fjm1 + 5 / 6 * fj + 1 / 3 * fjp1
+    fj_halfp3 = 1 / 3 * fj + 5 / 6 * fjp1 - 1 / 6 * fjp2
+
+    fj_halfp = w1 * fj_halfp1 + w2 * fj_halfp2 + w3 * fj_halfp3
+    dfj = fj_halfp[:,1:,:] - fj_halfp[:,0:-1,:]
+    return dfj
+
+@jit
+def WENO_L_y(f):
+
+    fj = f[:,3:-3,2:-3]
+    fjp1 = f[:,3:-3,3:-2]
+    fjp2 = f[:,3:-3,4:-1]
+    fjm1 = f[:,3:-3,1:-4]
+    fjm2 = f[:,3:-3,0:-5]
+
+    IS1 = 1 / 4 * jnp.power((fjm2 - 4 * fjm1 + 3 * fj), 2) + 13 / 12 * jnp.power((fjm2 - 2 * fjm1 + fj), 2)
+    IS2 = 1 / 4 * jnp.power((fjm1 - fjp1), 2) + 13 / 12 * jnp.power((fjm1 - 2 * fj + fjp1), 2)
+    IS3 = 1 / 4 * jnp.power((3 * fj - 4 * fjp1 + fjp2), 2) + 13 / 12 * jnp.power((fj - 2 * fjp1 + fjp2), 2)
+
+    alpha1 = C1 / jnp.power((eps + IS1), p)
+    alpha2 = C2 / jnp.power((eps + IS2), p)
+    alpha3 = C3 / jnp.power((eps + IS3), p)
+
+
+    w1 = alpha1 / (alpha1 + alpha2 + alpha3)
+    w2 = alpha2 / (alpha1 + alpha2 + alpha3)
+    w3 = alpha3 / (alpha1 + alpha2 + alpha3)
+
+    fj_halfp1 = 1 / 3 * fjm2 - 7 / 6 * fjm1 + 11 / 6 * fj
+    fj_halfp2 = -1 / 6 * fjm1 + 5 / 6 * fj + 1 / 3 * fjp1
+    fj_halfp3 = 1 / 3 * fj + 5 / 6 * fjp1 - 1 / 6 * fjp2
+
+    fj_halfp = w1 * fj_halfp1 + w2 * fj_halfp2 + w3 * fj_halfp3
+    dfj = fj_halfp[:,:,1:] - fj_halfp[:,:,0:-1]
+
+    return dfj
+
+@jit
+def WENO_R_x(f):
+
+    fj = f[:,3:-2,3:-3]
+    fjp1 = f[:,4:-1,3:-3]
+    fjp2 = f[:,5:,3:-3]
+    fjm1 = f[:,2:-3,3:-3]
+    fjm2 = f[:,1:-4,3:-3]
+
+    IS1 = 1 / 4 * jnp.power((fjp2 - 4 * fjp1 + 3 * fj), 2) + 13 / 12 * jnp.power((fjp2 - 2 * fjp1 + fj), 2)
+    IS2 = 1 / 4 * jnp.power((fjp1 - fjm1), 2) + 13 / 12 * jnp.power((fjp1 - 2 * fj + fjm1), 2)
+    IS3 = 1 / 4 * jnp.power((3 * fj - 4 * fjm1 + fjm2), 2) + 13 / 12 * jnp.power((fj - 2 * fjm1 + fjm2), 2)
+
+    alpha1 = C1 / jnp.power((eps + IS1), p)
+    alpha2 = C2 / jnp.power((eps + IS2), p)
+    alpha3 = C3 / jnp.power((eps + IS3), p)
+
+
+    w1 = alpha1 / (alpha1 + alpha2 + alpha3)
+    w2 = alpha2 / (alpha1 + alpha2 + alpha3)
+    w3 = alpha3 / (alpha1 + alpha2 + alpha3)
+
+    fj_halfm1 = 1 / 3 * fjp2 - 7 / 6 * fjp1 + 11 / 6 * fj
+    fj_halfm2 = -1 / 6 * fjp1 + 5 / 6 * fj + 1 / 3 * fjm1
+    fj_halfm3 = 1 / 3 * fj + 5 / 6 * fjm1 - 1 / 6 * fjm2
+
+    fj_halfm = w1 * fj_halfm1 + w2 * fj_halfm2 + w3 * fj_halfm3
+    dfj = (fj_halfm[:,1:,:] - fj_halfm[:,0:-1,:])
+
+    return dfj
+
+@jit
+def WENO_R_y(f):
+
+    fj = f[:,3:-3,3:-2]
+    fjp1 = f[:,3:-3,4:-1]
+    fjp2 = f[:,3:-3,5:]
+    fjm1 = f[:,3:-3,2:-3]
+    fjm2 = f[:,3:-3,1:-4]
+
+    IS1 = 1 / 4 * jnp.power((fjp2 - 4 * fjp1 + 3 * fj), 2) + 13 / 12 * jnp.power((fjp2 - 2 * fjp1 + fj), 2)
+    IS2 = 1 / 4 * jnp.power((fjp1 - fjm1), 2) + 13 / 12 * jnp.power((fjp1 - 2 * fj + fjm1), 2)
+    IS3 = 1 / 4 * jnp.power((3 * fj - 4 * fjm1 + fjm2), 2) + 13 / 12 * jnp.power((fj - 2 * fjm1 + fjm2), 2)
+
+    alpha1 = C1 / jnp.power((eps + IS1), p)
+    alpha2 = C2 / jnp.power((eps + IS2), p)
+    alpha3 = C3 / jnp.power((eps + IS3), p)
+
+
+    w1 = alpha1 / (alpha1 + alpha2 + alpha3)
+    w2 = alpha2 / (alpha1 + alpha2 + alpha3)
+    w3 = alpha3 / (alpha1 + alpha2 + alpha3)
+
+    fj_halfm1 = 1 / 3 * fjp2 - 7 / 6 * fjp1 + 11 / 6 * fj
+    fj_halfm2 = -1 / 6 * fjp1 + 5 / 6 * fj + 1 / 3 * fjm1
+    fj_halfm3 = 1 / 3 * fj + 5 / 6 * fjm1 - 1 / 6 * fjm2
+
+    fj_halfm = w1 * fj_halfm1 + w2 * fj_halfm2 + w3 * fj_halfm3
+    dfj = (fj_halfm[:,:,1:] - fj_halfm[:,:,0:-1])
+
+    return dfj
+
+
+
+@jit
+def HLLC_flux(Ul, Ur, aux_l, aux_r, ixy):
+
+    # 1. 计算左、右状态的原始变量
+    rhoL, uL, vL, YL, pL, aL = aux_func.U_to_prim(Ul, aux_l)
+    rhoR, uR, vR, YR, pR, aR = aux_func.U_to_prim(Ur, aux_r)
+
+    # 2. 计算法向速度
+    u_nL = jnp.where(ixy == 1, uL, vL)
+    u_nR = jnp.where(ixy == 1, uR, vR)
+
+    # 3. 计算波速 SL, SR（估计左、右最大波速）
+    SL = jnp.minimum(u_nL - aL, u_nR - aR)
+    SR = jnp.maximum(u_nL + aL, u_nR + aR)
+
+    # 4. 计算左、右物理通量
+    FL = flux(Ul, aux_l, ixy)
+    FR = flux(Ur, aux_r, ixy)
+
+    # 5. 计算星区速度 S_star（Toro公式）
+    numerator = pR - pL + rhoL * u_nL * (SL - u_nL) - rhoR * u_nR * (SR - u_nR)
+    denominator = rhoL * (SL - u_nL) - rhoR * (SR - u_nR) + 1e-16
+    S_star = numerator / denominator
+
+    # 6. 计算星区守恒变量 U*_L 和 U*_R
+    def calc_U_star(U, rho, u_n, S, S_star, p):
+        factor = rho * (S - u_n) / (S - S_star + 1e-16)
+
+        # 速度分量
+        u_ = U[1] / rho
+        v_ = U[2] / rho
+        # 速度向量
+        vel_vec = jnp.array([u_, v_])
+
+        # 法向单位向量
+        n_vec = jnp.array([1.0, 0.0]) if ixy == 1 else jnp.array([0.0, 1.0])
+
+        # 法向速度和切向速度分量
+        u_n_vec = jnp.dot(vel_vec, n_vec)
+        u_t_vec = vel_vec - u_n_vec * n_vec
+
+        # 星区速度向量：法向速度变为 S_star，切向速度保持不变
+        u_star_vec = S_star * n_vec + u_t_vec
+
+        # 星区总能量近似计算
+        E = U[3] / rho
+        kinetic_energy = 0.5 * (u_**2 + v_**2)
+        E_star = E + (S_star - u_n) * (S_star + p / (rho * (S - u_n)))
+
+        # 组装星区守恒量
+        U_star = jnp.stack([
+            factor * rho,
+            factor * rho * u_star_vec[0],
+            factor * rho * u_star_vec[1],
+            factor * rho * E_star,
+            factor * U[4] / rho  # 额外守恒量按比例缩放
+        ], axis=0)
+
+        return U_star
+
+    U_star_L = calc_U_star(Ul, rhoL, u_nL, SL, S_star, pL)
+    U_star_R = calc_U_star(Ur, rhoR, u_nR, SR, S_star, pR)
+
+    # 7. 计算星区通量
+    def F_star(U, U_star, S, S_star, F):
+        return F + S * (U_star - U)
+
+    F_star_L = F_star(Ul, U_star_L, SL, S_star, FL)
+    F_star_R = F_star(Ur, U_star_R, SR, S_star, FR)
+
+    # 8. 根据波速分段选择最终通量
+    cond1 = SL >= 0
+    cond2 = (SL < 0) & (S_star >= 0)
+    cond3 = (S_star < 0) & (SR > 0)
+    cond4 = SR <= 0
+
+    flux_out = jnp.where(cond1[None, ...], FL,
+                 jnp.where(cond2[None, ...], F_star_L,
+                  jnp.where(cond3[None, ...], F_star_R,
+                   jnp.where(cond4[None, ...], FR,
+                             jnp.zeros_like(FL)))))
+
+    return flux_out
+
+
+def weno5_HLLC(U, aux, dx, dy):
+
+    Ul = WENO_L_x(U)
+    Ur = WENO_R_x(U)
+    aux_l = WENO_L_x(aux)
+    aux_r = WENO_R_x(aux)
+    flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=1)  # x方向
+    dF = (flux_hllc[:, 1:, :] - flux_hllc[:, :-1, :]) / dx
+
+    Ul = WENO_L_y(U)
+    Ur = WENO_R_y(U)
+    aux_l = WENO_L_y(aux)
+    aux_r = WENO_R_y(aux)
+    flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=2)  # y方向
+    dG = (flux_hllc[:, :, 1:] - flux_hllc[:, :, :-1]) / dy
+
+    netflux = dF + dG
+
+    return -netflux
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
