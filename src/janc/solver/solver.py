4a2601f8ae58411a3608af16ee9f3c2f76203a87
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-28 15:27:59 +0800

    Update solver.py

--- src/janc/solver/solver.py
+++ src/janc/solver/solver.py
@@ -110,10 +110,10 @@ def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None, s
         _,T = aux_func.aux_to_thermo(U,aux)
         rho = U[0:1]
         Y = U[4:]/rho
-        drhoY = chemical.solve_implicit_rate(T[:,:,200:],rho[:,:,200:],Y[:,:,200:],dt)
+        drhoY = chemical.solve_implicit_rate(T[:,:,220:],rho[:,:,220:],Y[:,:,220:],dt)
         
         p1 = U[0:4,:,:]
-        p2 = jnp.concatenate([U[4:,:,:200],U[4:,:,200:] + drhoY],axis=2)
+        p2 = jnp.concatenate([U[4:,:,:220],U[4:,:,220:] + drhoY],axis=2)
         
         U_new = jnp.concatenate([p1,p2],axis=0)
         U_new = U_new*theta['AR'][0:1]
