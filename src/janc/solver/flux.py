9f591900cec619a04af7216bbd86f21a453124f9
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-28 17:02:02 +0800

    Update flux.py

--- src/janc/solver/flux.py
+++ src/janc/solver/flux.py
@@ -580,10 +580,12 @@ def weno5_HLLC(U, aux, dx, dy):
     aux_r = thermo.get_T(e_r,Y_r,T_r)
     Ul = jnp.concatenate([rho_l, rho_l*u_l, rho_l*v_l, rho_l*(e_l+0.5*(u_l**2+v_l**2)), rho_l*Y_l],axis=0)
     Ur = jnp.concatenate([rho_r, rho_r*u_r, rho_r*v_r, rho_r*(e_r+0.5*(u_r**2+v_r**2)), rho_r*Y_r],axis=0)
-    flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=1)  # x方向
-    dF = (flux_hllc[:, 1:, :] - flux_hllc[:, :-1, :]) / dx
+    #flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=1)  # x方向
+    #dF = (flux_hllc[:, 1:, :] - flux_hllc[:, :-1, :]) / dx
     #flux_hll = HLL_flux(Ul, Ur, aux_l, aux_r, ixy=1)  # x方向
     #dF = (flux_hll[:, 1:, :] - flux_hll[:, :-1, :]) / dx
+    flux_hllc_multi = HLLC_multi_flux(Ul, Ur, aux_l, aux_r, ixy=1)  # x方向
+    dF = (flux_hllc_multi[:, 1:, :] - flux_hllc_multi[:, :-1, :]) / dx
 
     var_p_l = WENO_L_y(var_p)
     var_p_r = WENO_R_y(var_p)
@@ -608,10 +610,12 @@ def weno5_HLLC(U, aux, dx, dy):
     aux_r = thermo.get_T(e_r,Y_r,T_r)
     Ul = jnp.concatenate([rho_l, rho_l*u_l, rho_l*v_l, rho_l*(e_l+0.5*(u_l**2+v_l**2)), rho_l*Y_l],axis=0)
     Ur = jnp.concatenate([rho_r, rho_r*u_r, rho_r*v_r, rho_r*(e_r+0.5*(u_r**2+v_r**2)), rho_r*Y_r],axis=0)
-    flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=2)  # y方向
-    dG = (flux_hllc[:, :, 1:] - flux_hllc[:, :, :-1]) / dy
+    #flux_hllc = HLLC_flux(Ul, Ur, aux_l, aux_r, ixy=2)  # y方向
+    #dG = (flux_hllc[:, :, 1:] - flux_hllc[:, :, :-1]) / dy
     #flux_hll = HLL_flux(Ul, Ur, aux_l, aux_r, ixy=2)  # y方向
     #dG = (flux_hll[:, :, 1:] - flux_hll[:, :, :-1]) / dy
+    flux_hllc_multi = HLLC_multi_flux(Ul, Ur, aux_l, aux_r, ixy=2)  # y方向
+    dG = (flux_hllc_multi[:, :, 1:] - flux_hllc_multi[:, :, :-1]) / dy
 
     netflux = dF + dG
 
@@ -659,6 +663,73 @@ def HLL_flux(Ul, Ur, aux_l, aux_r, ixy):
            jnp.where(SR <= 0, FR, flux_HLL))
 
 
+@jit
+def HLLC_multi_flux(Ul, Ur, aux_l, aux_r, ixy):
+    # 提取左右状态
+    rhoL, uL, vL, YL, pL, aL = aux_func.U_to_prim(Ul, aux_l)
+    rhoR, uR, vR, YR, pR, aR = aux_func.U_to_prim(Ur, aux_r)
+    gammaL = aux_l[0:1]
+    gammaR = aux_r[0:1]
+
+    EL = Ul[3:4,:,:] / rhoL
+    ER = Ur[3:4,:,:] / rhoR
+
+    # 用 ixy 控制提取的方向变量，避免 if
+    u_nL = jnp.where(ixy == 1, uL, vL)
+    u_nR = jnp.where(ixy == 1, uR, vR)
+    tangL = jnp.where(ixy == 1, vL, uL)
+    tangR = jnp.where(ixy == 1, vR, uR)
+
+    p_ba = 0.5*(pL + pR)
+    a_ba = 0.5*(aL + aR)
+    p_pvrs = 0.5*(pL + pR) - 0.5*(u_nR - u_nL)*p_ba*a_ba
+    p_star = jnp.maximum(jnp.zeroslike(p_pvrs), p_pvrs)
+
+    # 波速估计
+    qL = jnp.where(p_star <= pL, 1.0, jnp.sqrt(1 + 0.5 * (gammaL + 1) / gammaL * (p_star / pL - 1)))
+    qR = jnp.where(p_star <= pR, 1.0, jnp.sqrt(1 + 0.5 * (gammaR + 1) / gammaR * (p_star / pR - 1)))
+    SL = u_nL - aL*qL
+    SR = u_nR + aR*qR
+
+    # 中间波速 S*
+    S_star = (pR - pL + rhoL * u_nL * (SL - u_nL) - rhoR * u_nR * (SR - u_nR)) / (
+        rhoL * (SL - u_nL) - rhoR * (SR - u_nR)
+    )
+
+    # 左右通量
+    FL = flux(Ul, aux_l, ixy)
+    FR = flux(Ur, aux_r, ixy)
+
+    # 定义星区状态函数
+    def U_star(U, rho, u_n, tang, S, S_star, p, side):
+        factor = rho * (S - u_n) / (S - S_star + 1e-6)
+        E = U[3:4,:,:] / rho
+        Y = U[4:,:,:] / rho
+
+        # 构造星区守恒变量
+        Ust = jnp.concatenate([
+            factor,                              # rho*
+            factor * jnp.where(ixy == 1, S_star, tang),  # rho*u
+            factor * jnp.where(ixy == 1, tang, S_star),  # rho*v
+            factor * (E + (S_star - u_n) * (S_star + p / (rho * (S - u_n)))),  # rho*E*
+            factor * Y                           # rho*Y
+        ], axis=0)
+        return Ust
+
+    UL_star = U_star(Ul, rhoL, u_nL, tangL, SL, S_star, pL, 'L')
+    UR_star = U_star(Ur, rhoR, u_nR, tangR, SR, S_star, pR, 'R')
+
+    F_star_L = FL + SL * (UL_star - Ul)
+    F_star_R = FR + SR * (UR_star - Ur)
+
+    # HLLC分段选择
+    flux_HLLC_multi = jnp.where(
+        SL >= 0, FL,
+        jnp.where(S_star >= 0, F_star_L,
+        jnp.where(SR > 0, F_star_R, FR))
+    )
+
+    return flux_HLLC_multi
 
 
 
