efb0006a1e73620d0839ba86c47da71b4222daa6
wth666 <121304181+wth666@users.noreply.github.com>
2025-05-28 09:06:18 +0800

    Update solver.py

--- src/janc/solver/solver.py
+++ src/janc/solver/solver.py
@@ -102,6 +102,22 @@ def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None, s
         U_new = jnp.concatenate([p1,p2],axis=0)
         return jnp.concatenate([U_new,aux],axis=0)
 
+    def advance_source_termk(field,dt,theta):
+        U = field[0:-2]/theta['AR'][0:1]
+        aux = field[-2:]
+        aux = aux_func.update_aux(U, aux)
+        _,T = aux_func.aux_to_thermo(U,aux)
+        rho = U[0:1]
+        Y = U[4:]/rho
+        drhoY = chemical.solve_implicit_rate(T[:,:,220:],rho[:,:,220:],Y[:,:,220:],dt)
+        
+        p1 = U[0:4,:,:]
+        p2 = jnp.concatenate([U[4:,:,:220],U[4:,:,220:] + drhoY],axis=2)
+        
+        U_new = jnp.concatenate([p1,p2],axis=0)
+        U_new = U_new*theta['AR'][0:1]
+        return jnp.concatenate([U_new,aux],axis=0)
+
     
     if chem_solver_type == 'implicit':
         if solver_mode == 'amr':
@@ -123,7 +139,8 @@ def set_solver(thermo_set, boundary_set, source_set = None, nondim_set = None, s
                 @jit    
                 def advance_one_step(field,dx,dy,dt,theta=None):
                     field_adv = advance_flux(field,dx,dy,dt,theta)
-                    field = advance_source_term(field_adv,dt)
+                    #field = advance_source_term(field_adv,dt)
+                    field = advance_source_termk(field_adv,dt,theta)
                     return field
     else:
         if solver_mode == 'amr':
