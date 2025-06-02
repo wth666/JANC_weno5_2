214bf8af07fb32f08cc226319b1c3ea728be52ba
wth666 <121304181+wth666@users.noreply.github.com>
2025-06-02 11:00:34 +0800

    Update solver.py

--- src/janc/solver/solver.py
+++ src/janc/solver/solver.py
@@ -4,6 +4,7 @@ from ..solver import aux_func
 from .flux import weno5
 from .flux import weno5_w
 from .flux import weno5_HLLC
+from .flux import weno5_SW
 from ..thermodynamics import thermo
 from ..thermodynamics import chemical
 from ..boundary import boundary
