6d4aacaeb6c0cca25f9da513dcc19371d4e1a531
wth666 <121304181+wth666@users.noreply.github.com>
2025-08-08 23:01:51 +0800

    Update slip_wall.py

--- src/janc/boundary/slip_wall.py
+++ src/janc/boundary/slip_wall.py
@@ -3,8 +3,8 @@ import jax.numpy as jnp
 def left(U_bd, aux_bd):
     U_bd_ghost = jnp.concatenate([U_bd[:,2:3,:],U_bd[:,1:2,:],U_bd[:,0:1,:]],axis=1)
     
-    U_bd_ghost = U_bd_ghost.at[3,:,0].set(U_bd_ghost[3,:,0] - 0.5*U_bd_ghost[0,:,0]*(U_bd_ghost[2,:,0]/U_bd_ghost[0,:,0])**2
-    U_bd_ghost = U_bd_ghost.at[3,:,-1].set(U_bd_ghost[3,:,-1] - 0.5*U_bd_ghost[0,:,-1]*(U_bd_ghost[2,:,-1]/U_bd_ghost[0,:,-1])**2
+    U_bd_ghost = U_bd_ghost.at[3,:,0].set(U_bd_ghost[3,:,0] - 0.5*U_bd_ghost[0,:,0]*(U_bd_ghost[2,:,0]/U_bd_ghost[0,:,0])**2)
+    U_bd_ghost = U_bd_ghost.at[3,:,-1].set(U_bd_ghost[3,:,-1] - 0.5*U_bd_ghost[0,:,-1]*(U_bd_ghost[2,:,-1]/U_bd_ghost[0,:,-1])**2)
     U_bd_ghost = U_bd_ghost.at[2,:,0].set(0)
     U_bd_ghost = U_bd_ghost.at[2,:,-1].set(0)
               
@@ -30,3 +30,4 @@ def up(U_bd, aux_bd):
     U_bd_ghost = U_bd_ghost.at[2].multiply(-1)
 
     return U_bd_ghost, aux_bd_ghost
+
